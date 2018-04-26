6208be3f7505579c924ac45e09a916fda85c9486
Tiago Massita <massita@MacBook-Pro-de-Tiago.local>
2018-04-26 10:53:45 -0300

    Creating the model and service to get upcoming movies

--- app/src/main/java/com/massita/upmovies/api/ApiClient.kt
+++ app/src/main/java/com/massita/upmovies/api/ApiClient.kt
@@ -1,17 +1,22 @@
 package com.massita.upmovies.api
 
+import com.massita.upmovies.api.service.MovieService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiClient {
 
     companion object {
-        const val BASE_URL = ""
+        const val BASE_URL = "https://api.themoviedb.org"
     }
 
     private val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
+    fun getMovieService() : MovieService {
+        return retrofit.create(MovieService::class.java)
+    }
 
 }
--- app/src/main/java/com/massita/upmovies/api/service/MovieService.kt
+++ app/src/main/java/com/massita/upmovies/api/service/MovieService.kt
@@ -1,6 +1,12 @@
 package com.massita.upmovies.api.service
 
+import com.massita.upmovies.api.model.UpcomingList
+import io.reactivex.Observable
+import retrofit2.http.GET
+
 interface MovieService {
 
-    fun getUpcoming()
+    @GET("3/movie/upcoming")
+    fun getUpcoming() : Observable<UpcomingList>
+
 }
