c145958830391d4791d74d9e0381dbece83ab2af
Tiago Massita <massita@MacBook-Pro-de-Tiago.local>
2018-04-27 11:28:06 -0300

    Added a placeholder to movies covers

--- app/src/main/java/com/massita/upmovies/extension/ViewExtension.kt
+++ app/src/main/java/com/massita/upmovies/extension/ViewExtension.kt
@@ -1,19 +1,18 @@
 package com.massita.upmovies.extension
 
 import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.support.v7.graphics.Palette
 import android.view.View
 import android.widget.ImageView
-import android.widget.LinearLayout
+import com.massita.upmovies.R
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
 fun ImageView.load(imagePath: String, callback: Callback) {
     Picasso.get()
             .load(imagePath)
-            .noPlaceholder()
+            .placeholder(R.drawable.upcoming_movies_placeholder)
             .into(this, callback)
 }
 
