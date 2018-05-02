8e11296396e82a727b92409f9921167c03522407
Tiago Massita <massita@MacBook-Pro-de-Tiago.local>
2018-05-02 13:20:23 -0300

    Created movie details model

--- app/src/main/java/com/massita/upmovies/api/model/Movie.kt
+++ app/src/main/java/com/massita/upmovies/api/model/Movie.kt
@@ -6,18 +6,11 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Movie(
-        @SerializedName("poster_path") val posterPath: String?,
-        @SerializedName("adult") val adult: Boolean,
         @SerializedName("overview") val overview: String,
         @SerializedName("release_date") val releaseDate: String,
-        @SerializedName("genre_ids") val genderIds: List<Int>,
         @SerializedName("id") val id: Int,
         @SerializedName("original_title") val originalTitle: String,
         @SerializedName("original_language") val originalLanguage: String,
         @SerializedName("title") val title: String,
-        @SerializedName("backdrop_path") val backdropPath: String?,
-        @SerializedName("popularity") val popularity: Float,
-        @SerializedName("vote_count") val voteCount: Int,
-        @SerializedName("video") val video: Boolean,
-        @SerializedName("vote_average") val voteAverage: Float
+        @SerializedName("backdrop_path") val backdropPath: String?
 ) : Parcelable
